751b22c2f2fde9fda7466e8ad9183c425f92a0bd
Sahina Akther <mazharul2901012@gmail.com>
2025-09-06 20:11:49 +0000

    Change TikTok logo as same to same

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -9,15 +9,18 @@ import type { SocialPlatform } from '@/lib/types';
 const TikTokIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
+    viewBox="0 0 2859 3333"
+    shapeRendering="geometricPrecision"
+    textRendering="geometricPrecision"
+    imageRendering="optimizeQuality"
+    fillRule="evenodd"
+    clipRule="evenodd"
     fill="currentColor"
     {...props}
   >
-    <path d="M22.05 8.63h-2.6v8.83c0 2.45-1.98 4.43-4.43 4.43s-4.43-1.98-4.43-4.43s1.98-4.43 4.43-4.43c.48 0 .94.08 1.38.22V5.59c-.46-.11-.94-.17-1.44-.17C10.7 5.42 7.1 9.02 7.1 13.46c0 4.44 3.6 8.04 8.04 8.04c4.44 0 8.04-3.6 8.04-8.04V8.63h-1.13Z"/>
-    <path d="M19.45 2.55H14.1v10.51c0 .28.23.51.51.51s.51-.23.51-.51V2.55h-2.6v8.83c0 1.6-.63 3.06-1.68 4.12c-.1.1-.15.24-.15.38c0 .32.26.58.58.58c.24 0 .46-.15.55-.37c1.1-2.52 4.14-2.82 4.14-7.55V2.55Z"/>
+    <path d="M2081 0c55 473 319 755 778 785v532c-266 26-499-61-770-225v995c0 1264-1378 1659-1932 753-356-583-138-1606 1004-1647v561c-87 14-180 36-265 65-254 86-458 249-622 446-334 393-372 999-94 1462s1035 596 1554 150c219-189 344-434 408-711h-413v-561h744C2829 1233 2812 559 2081 0z" />
   </svg>
 );
-
 
 const iconMap: Record<SocialPlatform, React.ElementType> = {
   instagram: Instagram,
